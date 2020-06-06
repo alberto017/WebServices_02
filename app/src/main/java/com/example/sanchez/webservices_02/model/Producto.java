8325213a3a6391a6467f4bb58d3bf24891fd4ca7
Alberto017 <xavihdz09@gmail.com>
2020-06-05 18:34:25 -0700

    feat: List product

---
+++ app/src/main/java/com/example/sanchez/webservices_02/model/Producto.java
@@ -0,0 +1,41 @@
+package com.example.sanchez.webservices_02.model;
+
+public class Producto {
+
+    private int codigo;
+    private String nombre;
+    private String precio;
+    private String fabricante;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+}//Producto
